b2fb6baec98885fcd169ad44e0d043fb993d713f
DmitryTXCMN <81518929+DmitryTXCMN@users.noreply.github.com>
2021-03-29 01:10:12 +0300

    Create Program.cs

---
+++ Скобочка/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace BracketStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> brackets = new Stack<char>();
+            string input;
+            using (StreamReader sr = new StreamReader(@"C:\Users\draku\source\repos\BracketStack\BracketStack\text.txt"))
+            {
+                input = sr.ReadLine();
+            }
+            foreach (char symbol in input)
+            {
+                switch (symbol)
+                {
+                    case '(':
+                        brackets.Push('(');
+                        break;
+                    case '[':
+                        brackets.Push('[');
+                        break;
+                    case '{':
+                        brackets.Push('{');
+                        break;
+                    case ')':
+                        if (brackets.Peek() != '(')
+                            throw new Exception();
+                        brackets.Pop();
+                        break;
+                    case ']':
+                        if (brackets.Peek() != '[')
+                            throw new Exception();
+                        brackets.Pop();
+                        break;
+                    case '}':
+                        if (brackets.Peek() != '{')
+                            throw new Exception();
+                        brackets.Pop();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.WriteLine("all good");
+        }
+    }
+}
